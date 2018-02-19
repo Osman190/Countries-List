038f166b59c421e0846838a7acbb8f9e361357cc
Elbehary <ahmedelbehary0@gmail.com>
2018-02-19 16:39:57 +0100

    Fatch and other Stuff

---
+++ main.js
@@ -0,0 +1,56 @@
+// class countriesList{
+//   constructor(){
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open('GET', 'https://restcountries.eu/rest/v2/all', false);
+//     xhttp.send();
+//     this.countries = JSON.parse(xhttp.responseText)
+//   }
+
+//   printCountries(){
+//     console.log(this.countries)
+//   }
+// }
+
+// const objconutries1 = new countriesList();
+// objconutries1.printCountries()
+
+let body = document.querySelector('body');
+
+let url = 'https://restcountries.eu/rest/v2/all'
+let countriesList = [];
+  function allCountry(){ 
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(countries =>{
+        countriesList = countries;
+        overview(countriesList);
+    })
+  }
+
+  
+  function overview(countriesList) {
+    let html = '';
+    let i = 1
+    let tableBody = document.getElementById('tableBody')
+    let counter = 0;
+    
+     countriesList.forEach(country => {
+      counter += country.population
+     })
+
+     countriesList.forEach(country => {
+       html += `<tr>
+              <td>${i}</td>
+              <td width=20%>${country.name}<td>
+              <td width=20%><img src='${country.flag}' width=30%</td>
+              <td width=20%>${country.population}</td>
+              <td width=20%>${((country.population /counter)  * 100).toFixed(3)} %</td>
+              </tr>`;
+              i++
+     })
+     tableBody.innerHTML = html;
+   }
+
+   allCountry()

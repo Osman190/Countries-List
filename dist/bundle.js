e4cdaded5956bd244c72a8aea14e4c9547447264
Elbehary <ahmedelbehary0@gmail.com>
2018-03-02 15:51:19 +0100

    WIP

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,181 @@
-!function(t){var o={};function n(e){if(o[e])return o[e].exports;var r=o[e]={i:e,l:!1,exports:{}};return t[e].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=t,n.c=o,n.d=function(t,o,e){n.o(t,o)||Object.defineProperty(t,o,{configurable:!1,enumerable:!0,get:e})},n.r=function(t){Object.defineProperty(t,"__esModule",{value:!0})},n.n=function(t){var o=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(o,"a",o),o},n.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},n.p="",n(n.s=0)}([function(t,o){document.querySelector("body");let n=[];fetch("https://restcountries.eu/rest/v2/all").then(t=>t.json()).then(t=>{(function(t){let o="",n=1,e=document.getElementById("tableBody"),r=0;t.forEach(t=>{r+=t.population}),"sort=asc"===window.location.search.substr(1)?t.sort((t,o)=>o.population-t.population):"sort=desc"===window.location.search.substr(1)&&t.sort((t,o)=>t.population-o.population),t.forEach(t=>{o+=`<tr>\n              <td>${n}</td>\n              <td width=20%>${t.name}<td>\n              <td width=20%><img src='${t.flag}' width=30%</td>\n              <td width=20%>${t.population}</td>\n              <td width=20%\n              <b>${(t.population/r*100).toFixed(5)} %</td>\n              </tr>`,n++}),e.innerHTML=o})(n=t),console.log(n)})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+// class countriesList{
+//   constructor(){
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open('GET', 'https://restcountries.eu/rest/v2/all', false);
+//     xhttp.send();
+//     this.countries = JSON.parse(xhttp.responseText)
+//   }
+
+//   printCountries(){
+//     console.log(this.countries)
+//   }
+// }
+
+// const objconutries1 = new countriesList();
+// objconutries1.printCountries()
+
+let body = document.querySelector('body');
+
+let url = 'https://restcountries.eu/rest/v2/all'
+let countriesList = [];
+  function allCountry(){ 
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(countries =>{
+        countriesList = countries;
+        overview(countriesList);
+        console.log(countriesList)
+    })
+  }
+  
+  function overview(countriesList) {
+
+    function creatLayout() { cx0
+      if (document.getElementById("searchcountry").value) {
+        console.log(document.getElementById("searchcountry").value)
+        countriesList = countriesList.filter(i => !i.name.toLowerCase().includes(document.getElementById("searchcountry").value.toLowerCase()))
+
+      } else {
+        countriesList = countriesList
+      }
+    }
+
+    function sort() {  
+      if (window.location.search.substr(1) === "sort=asc") {
+        countriesList.sort((a, b) => b.population - a.population)
+      } else if (window.location.search.substr(1) === "sort=desc") {
+        countriesList.sort((a, b) => a.population - b.population)
+      }
+    }
+    sort()
+
+    function initMap(lat,lng,zoom) {
+      const coord = {
+        lat: lat,
+        lng: lng
+      }
+      const map = new google.maps.Map(document.getElementById('map'),
+      {
+        zoom: zoom, 
+        center: coord
+      })
+      const marker = new google.maps.Marker({
+        position:coord, map:map
+      })
+    }
+
+    function searchButton() {
+      let button = document.getElementById('searchcountry');
+      button.addEventListener('keyup', (e) => {
+        e.preventDefault()
+        creatLayout(countriesList)
+      })
+    }
+    searchButton()
+
+    document.getElementById('tableBody').addEventListener('click', function(e){
+      const {target} = e;
+      const coord = target.getAttibute('data-id');
+      const final = target.spilt(',');
+      initMap(Number(final[0]), Number(final[1]), 6);
+      console.log(e.target);
+    })
+
+
+    let html = '';
+    let i = 1
+    let tableBody = document.getElementById('tableBody')
+    let counter = 0;
+     countriesList.forEach(country => {
+      counter += country.population
+     })
+     countriesList.forEach(country => {
+       html += `<tr>
+              <td>${i}</td>
+              <td width=20%>${country.name}<td>
+              <td width=20%><img src='${country.flag}' width=30%</td>
+              <td width=20%>${country.population}</td>
+              <td width=20%
+              <b>${((country.population /counter) * 100).toFixed(5)} %</td>
+              </tr>`;
+              i++
+     })
+     tableBody.innerHTML = html;
+   }
+
+
+   allCountry()
+
+/***/ })
+/******/ ]);
--- src/index.js
+++ src/index.js
@@ -31,6 +31,59 @@ let countriesList = [];
   }
   
   function overview(countriesList) {
+
+    function creatLayout() { cx0
+      if (document.getElementById("searchcountry").value) {
+        console.log(document.getElementById("searchcountry").value)
+        countriesList = countriesList.filter(i => !i.name.toLowerCase().includes(document.getElementById("searchcountry").value.toLowerCase()))
+
+      } else {
+        countriesList = countriesList
+      }
+    }
+
+    function sort() {  
+      if (window.location.search.substr(1) === "sort=asc") {
+        countriesList.sort((a, b) => b.population - a.population)
+      } else if (window.location.search.substr(1) === "sort=desc") {
+        countriesList.sort((a, b) => a.population - b.population)
+      }
+    }
+    sort()
+
+    function initMap(lat,lng,zoom) {
+      const coord = {
+        lat: lat,
+        lng: lng
+      }
+      const map = new google.maps.Map(document.getElementById('map'),
+      {
+        zoom: zoom, 
+        center: coord
+      })
+      const marker = new google.maps.Marker({
+        position:coord, map:map
+      })
+    }
+
+    function searchButton() {
+      let button = document.getElementById('searchcountry');
+      button.addEventListener('keyup', (e) => {
+        e.preventDefault()
+        creatLayout(countriesList)
+      })
+    }
+    searchButton()
+
+    document.getElementById('tableBody').addEventListener('click', function(e){
+      const {target} = e;
+      const coord = target.getAttibute('data-id');
+      const final = target.spilt(',');
+      initMap(Number(final[0]), Number(final[1]), 6);
+      console.log(e.target);
+    })
+
+
     let html = '';
     let i = 1
     let tableBody = document.getElementById('tableBody')
@@ -38,11 +91,6 @@ let countriesList = [];
      countriesList.forEach(country => {
       counter += country.population
      })
-       if (window.location.search.substr(1) === "sort=asc") {
-         countriesList.sort((a, b) => b.population - a.population)
-       } else if (window.location.search.substr(1) === "sort=desc") {
-         countriesList.sort((a, b) => a.population - b.population)
-       }
      countriesList.forEach(country => {
        html += `<tr>
               <td>${i}</td>
@@ -54,8 +102,8 @@ let countriesList = [];
               </tr>`;
               i++
      })
-     
      tableBody.innerHTML = html;
    }
+
 
    allCountry()
